e66323389e20cfc0740552ecc2c9f61be3a010ac
GhostQut <69810133+GhostQut@users.noreply.github.com>
2023-08-23 14:38:35 +0300

    Update LoaderV1.lua

--- LoaderV1.lua
+++ LoaderV1.lua
@@ -18,3 +18,10 @@ for id, url in next, list do
     end
 end
 
+local A = loadstring(game:HttpGet("https://raw.githubusercontent.com/GhostQut/YammiHub/main/Games"))();
+
+for i, v in next, Games do
+    if i == game.PlaceId then
+        loadstring(game:HttpGet(v))()
+    end
+end
