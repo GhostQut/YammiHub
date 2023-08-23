189538b70e4d40fa963488c0319e9ad40cf053b1
GhostQut <69810133+GhostQut@users.noreply.github.com>
2023-08-23 14:00:03 +0300

    Update LoaderV1.lua

--- LoaderV1.lua
+++ LoaderV1.lua
@@ -1,10 +1,7 @@
 repeat task.wait() until game:IsLoaded()
 
-local A = loadstring(game:HttpGet("https://raw.githubusercontent.com/GhostQut/YammiHub/main/Games.lua"))()
+local A = loadstring(game:HttpGet("https://raw.githubusercontent.com/GhostQut/YammiHub/main/Games"))()
 
-if A[game.placeId] then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/GhostQut/Scripts/main/"..A[game.placeId][2]..".lua"))()
-end
 for i, v in next, Games do
     if i == game.PlaceId then
         loadstring(game:HttpGet(v))()
