9ac2d9a211a6040268c80f852a3a72ece801b638
GhostQut <69810133+GhostQut@users.noreply.github.com>
2023-08-23 14:41:42 +0300

    Update LoaderV1.lua

--- LoaderV1.lua
+++ LoaderV1.lua
@@ -7,21 +7,12 @@ local list = {
 	["4442272183"] = "https://raw.githubusercontent.com/1201for/V.G-Hub/main/Blox-Fruit";
 	["2753915549"] = "https://raw.githubusercontent.com/1201for/V.G-Hub/main/Blox-Fruit";
 	["8737602449"] = "https://raw.githubusercontent.com/GhostQut/Scripts/main/PLSDONATE.lua";
-	["301549746"] = {
-		[1] = "Counter Blox",
-		[2] = "CBRO"
 }
-    
+
 for id, url in next, list do
     if string.find(id, game.PlaceId) then
         loadstring(game:HttpGet(url))(); break
     end
 end
 
-local A = loadstring(game:HttpGet("https://raw.githubusercontent.com/GhostQut/YammiHub/main/Games"))();
 
-for i, v in next, Games do
-    if i == game.PlaceId then
-        loadstring(game:HttpGet(v))()
-    end
-end
