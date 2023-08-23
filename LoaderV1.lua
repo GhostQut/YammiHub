7c3c687574b95895e6c72ea90aef197a8a60fbe6
GhostQut <69810133+GhostQut@users.noreply.github.com>
2023-08-23 13:43:58 +0300

    Update LoaderV1.lua

--- LoaderV1.lua
+++ LoaderV1.lua
@@ -1,32 +1,5 @@
-local supportedGames = {
-	[12761410397] = {
-		[1] = "SPTSAU",
-		[2] = "SPTS:AU"
-},
-	[11545598432] = {
-		[1] = "AnimeFightingSimulatorX",
-		[2] = "AFSX"
-},
-	[8554378337] = {
-		[1] = "WeaponFightingSimulator",
-		[2] = "WFS"
-},
-	[2753915549] = {
-		[1] = "BloxFruits",
-		[2] = "BX"
-},
-	[8737602449] = {
-		[1] = "PLSDONATE",
-		[2] = "PD"
-},
-	[914010731] = {
-		[1] = "Ro-Ghoul",
-		[2] = "RoGhoul"
-},
-	[301549746] = {
-		[1] = "Counter Blox",
-		[2] = "CBRO"
-}
+local A = loadstring(game:HttpGet("https://raw.githubusercontent.com/GhostQut/YammiHub/main/Games.lua"))()
+
 if supportedGames[game.placeId] then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/GhostQut/Scripts/main/"..supportedGames[game.placeId][2]..".lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/GhostQut/Scripts/main/"..A[game.placeId][2]..".lua"))()
 end
