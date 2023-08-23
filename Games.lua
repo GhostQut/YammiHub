4f0e575dda15b45a1708f96ff8fe0d89e9e859c9
GhostQut <69810133+GhostQut@users.noreply.github.com>
2023-08-23 13:43:11 +0300

    Create Games.lua

---
+++ Games.lua
@@ -0,0 +1,29 @@
+local supportedGames = {
+	[12761410397] = {
+		[1] = "SPTSAU",
+		[2] = "SPTS:AU"
+},
+	[11545598432] = {
+		[1] = "AnimeFightingSimulatorX",
+		[2] = "AFSX"
+},
+	[8554378337] = {
+		[1] = "WeaponFightingSimulator",
+		[2] = "WFS"
+},
+	[2753915549] = {
+		[1] = "BloxFruits",
+		[2] = "BX"
+},
+	[8737602449] = {
+		[1] = "PLSDONATE",
+		[2] = "PD"
+},
+	[914010731] = {
+		[1] = "Ro-Ghoul",
+		[2] = "RoGhoul"
+},
+	[301549746] = {
+		[1] = "Counter Blox",
+		[2] = "CBRO"
+}
