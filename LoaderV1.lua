472b583bf9d4048868b24c05574018d5410e029e
GhostQut <69810133+GhostQut@users.noreply.github.com>
2023-08-23 13:58:06 +0300

    Update LoaderV1.lua

--- LoaderV1.lua
+++ LoaderV1.lua
@@ -5,3 +5,8 @@ local A = loadstring(game:HttpGet("https://raw.githubusercontent.com/GhostQut/Ya
 if A[game.placeId] then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/GhostQut/Scripts/main/"..A[game.placeId][2]..".lua"))()
 end
+for i, v in next, Games do
+    if i == game.PlaceId then
+        loadstring(game:HttpGet(v))()
+    end
+end
