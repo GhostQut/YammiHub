353282b0096de7566af205e45b67526403a9ddda
GhostQut <69810133+GhostQut@users.noreply.github.com>
2023-08-23 13:55:20 +0300

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -1,16 +1,8 @@
 local supportedGames = {
-	[12761410397] = {
-		[1] = "SPTSAU",
-		[2] = "SPTS:AU"
-},
+	[12761410397] = {https://raw.githubusercontent.com/GhostQut/Scripts/main/SPTSAU.lua},
 	[11545598432] = {
-		[1] = "AnimeFightingSimulatorX",
-		[2] = "AFSX"
-},
-	[8554378337] = {
-		[1] = "WeaponFightingSimulator",
-		[2] = "WFS"
-},
+		https://raw.githubusercontent.com/GhostQut/Scripts/main/AFSX.lua},
+	[8554378337] = {https://raw.githubusercontent.com/GhostQut/Scripts/main/WFS.lua},
 	[2753915549] = {
 		[1] = "BloxFruits",
 		[2] = "BX"
@@ -19,10 +11,7 @@ local supportedGames = {
 		[1] = "PLSDONATE",
 		[2] = "PD"
 },
-	[914010731] = {
-		[1] = "Ro-Ghoul",
-		[2] = "RoGhoul"
-},
+	[914010731] = {https://raw.githubusercontent.com/GhostQut/Scripts/main/RoGhoul.lua},
 	[301549746] = {
 		[1] = "Counter Blox",
 		[2] = "CBRO"
