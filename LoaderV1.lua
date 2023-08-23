5cbd728a0de6a54da74eb7b9e796ca116e6db6ad
GhostQut <69810133+GhostQut@users.noreply.github.com>
2023-08-23 14:31:25 +0300

    Update LoaderV1.lua

--- LoaderV1.lua
+++ LoaderV1.lua
@@ -1,9 +1,4 @@
-local list = {
-    ["12761410397"] = 'https://raw.githubusercontent.com/GhostQut/Scripts/main/SPTSAU.lua';
-    ["11545598432"] = 'https://raw.githubusercontent.com/GhostQut/Scripts/main/AFSX.lua';
-	["8554378337"] = 'https://raw.githubusercontent.com/GhostQut/Scripts/main/WFS.lua';
-    ["914010731"] = 'https://raw.githubusercontent.com/GhostQut/Scripts/main/RoGhoul.lua';
-}
+local A = loadstring(game:HttpGet("https://raw.githubusercontent.com/GhostQut/YammiHub/main/Games"))();
 
 for id, url in next, list do
     if string.find(id, game.PlaceId) then
@@ -11,10 +6,3 @@ for id, url in next, list do
     end
 end
 
-local A = loadstring(game:HttpGet("https://raw.githubusercontent.com/GhostQut/YammiHub/main/Games"))();
-
-for i, v in next, Games do
-    if i == game.PlaceId then
-        loadstring(game:HttpGet(v))()
-    end
-end
