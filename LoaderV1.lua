c49b1bad11e16203c95d8bb1d853ccf26eeb7c98
GhostQut <69810133+GhostQut@users.noreply.github.com>
2023-08-23 14:26:09 +0300

    Update LoaderV1.lua

--- LoaderV1.lua
+++ LoaderV1.lua
@@ -1,4 +1,15 @@
-repeat task.wait() until game:IsLoaded();
+local list = {
+    ["12761410397"] = 'https://raw.githubusercontent.com/GhostQut/Scripts/main/SPTSAU.lua';
+    ["11545598432"] = 'https://raw.githubusercontent.com/GhostQut/Scripts/main/AFSX.lua';
+	["8554378337"] = 'https://raw.githubusercontent.com/GhostQut/Scripts/main/WFS.lua';
+    ["914010731"] = 'https://raw.githubusercontent.com/GhostQut/Scripts/main/RoGhoul.lua';
+}
+
+for id, url in next, list do
+    if string.find(id, game.PlaceId) then
+        loadstring(game:HttpGet(url))(); break
+    end
+end
 
 local A = loadstring(game:HttpGet("https://raw.githubusercontent.com/GhostQut/YammiHub/main/Games"))();
 
