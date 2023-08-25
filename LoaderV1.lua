b3f5036b438477a68dccfeaa17bd76f8d5c9a7da
GhostQut <69810133+GhostQut@users.noreply.github.com>
2023-08-25 10:54:07 +0300

    Update LoaderV1.lua

--- LoaderV1.lua
+++ LoaderV1.lua
@@ -3,10 +3,14 @@ local list = {
 	["11545598432"] = 'https://raw.githubusercontent.com/GhostQut/Scripts/main/AFSX.lua';
 	["8554378337"] = 'https://raw.githubusercontent.com/GhostQut/Scripts/main/WFS.lua';
 	["914010731"] = 'https://raw.githubusercontent.com/GhostQut/Scripts/main/RoGhoul.lua';
-	["2753915549"] = "https://raw.githubusercontent.com/1201for/V.G-Hub/main/Blox-Fruit";
-	["4442272183"] = "https://raw.githubusercontent.com/1201for/V.G-Hub/main/Blox-Fruit";
-	["2753915549"] = "https://raw.githubusercontent.com/1201for/V.G-Hub/main/Blox-Fruit";
-	["8737602449"] = "https://raw.githubusercontent.com/GhostQut/Scripts/main/PLSDONATE.lua";
+	["2753915549"] = 'https://raw.githubusercontent.com/1201for/V.G-Hub/main/Blox-Fruit';
+	["4442272183"] = 'https://raw.githubusercontent.com/1201for/V.G-Hub/main/Blox-Fruit';
+	["2753915549"] = 'https://raw.githubusercontent.com/1201for/V.G-Hub/main/Blox-Fruit';
+	["8737602449"] = 'https://raw.githubusercontent.com/GhostQut/Scripts/main/PLSDONATE.lua';
+	["6284583030"] = 'https://raw.githubusercontent.com/1201for/V.G-hub-Part-Two/main/Pet-Simulator-X';
+	["7722306047"] = 'https://raw.githubusercontent.com/1201for/V.G-hub-Part-Two/main/Pet-Simulator-X';
+	["10321372166"] = 'https://raw.githubusercontent.com/1201for/V.G-hub-Part-Two/main/Pet-Simulator-X';
+	["301549746"] = 
 }
 
 for id, url in next, list do
